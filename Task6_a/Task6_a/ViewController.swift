9cc3d08451fcc4553ee5c0ed10c752fe0b725751
egorzainullin <egor.zainullin@outlook.com>
2021-04-16 15:10:13 +0300

    task 1 done

--- Task6_a/Task6_a/ViewController.swift
+++ Task6_a/Task6_a/ViewController.swift
@@ -13,7 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBOutlet weak var namesLabel: UILabel!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBAction func onTouchUp(_ sender: Any) {
+        switch (nameTextField.text, namesLabel.text) {
+        case (""?, let names?):
+            namesLabel.text = names
+        case (let name?, let names?):
+            namesLabel.text = name + " " + names
+        case (let name?, _):
+            namesLabel.text = name
+        case (_, let names?):
+            namesLabel.text = names
+        default:
+            namesLabel.text = ""
+        }
+    }
 
 }
 
