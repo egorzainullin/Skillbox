7cc8d1eaa034097087c762bc8dda3e2aae5a3603
egorzainullin <egor.zainullin@outlook.com>
2021-12-26 15:09:55 +0300

    try to do 3 task

--- Task10/Task10/ViewController.swift
+++ Task10/Task10/ViewController.swift
@@ -10,19 +10,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let cell = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 80))
-        let label = UILabel(frame: cell.bounds)
-        label.text = "Text"
-        label.textAlignment = .center
-        cell.addSubview(label)
-        scrollView.addSubview(cell)
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
 
-    }
-
-
+      
 }
 
