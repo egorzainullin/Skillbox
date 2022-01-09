e0537d437378182d12cf0058a28f77f215701266
egorzainullin <egor.zainullin@outlook.com>
2022-01-09 17:35:01 +0300

    added cell controller

---
+++ Task12/Task12/CharacterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CharacterViewController.swift
+//  Task12
+//
+//  Created by Егор Зайнуллин on 09.01.2022.
+//
+
+import UIKit
+
+class CharacterViewController: UIViewController {
+
+    var id: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Task12/Task12/CharacterableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterableViewCell.swift
+//  Task12
+//
+//  Created by Егор Зайнуллин on 09.01.2022.
+//
+
+import UIKit
+
+class CharacterableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+}
