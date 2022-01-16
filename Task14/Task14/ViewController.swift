9486db6f1bb4375202a3288dda9bd56e69e7f0dc
egorzainullin <egor.zainullin@outlook.com>
2022-01-16 11:41:24 +0300

    added task 14

---
+++ Task14/Task14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task14
+//
+//  Created by Егор Зайнуллин on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
